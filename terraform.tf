e679f7b7ad358f420eabf12b1cb28df2650725a8
Steven-lsz <liushizhaosingapore@gmail.com>
2023-06-22 15:33:28 +0800

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,12 @@
 terraform {
 
-  cloud {
-    organization = "organization-name"
+ /* cloud {
+    organization = "steven-lsz"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
-  }
+  }*/
 
   required_providers {
     aws = {
